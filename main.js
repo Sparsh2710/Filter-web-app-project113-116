00271bc03a86d3f91578d8999afab12f98b55ff4
Sparsh2710 <70764422+Sparsh2710@users.noreply.github.com>
2020-11-15 13:59:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ function preload() {
 function setup() {
     Canvas = createCanvas(300, 300);
     Canvas.center();
+    Video =createCapture(VIDEO);
+    Video.size(300, 300);
+    Video.hide();
+    poseNet = ml5.poseNet(Video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose X = " + results[0].pose.nose.x);
+        console.log("nose Y = " + results[0].pose.nose.y);
+    }
 }
 
 function draw() {
-    
+    image(img, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+    save('FilterImage.jpg')
 }
